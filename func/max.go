2e97d1037eb97277e5c86d823f44bbf26f17b6f2
luo0412 <1095847440@qq.com>
2017-03-05 23:00:12 +0800

    add sth

---
+++ func/max.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+
+/* 函数返回两个数的最大值 */
+func max(num1, num2 int) int {
+   /* 声明局部变量 */
+   var result int
+
+   if (num1 > num2) {
+      result = num1
+   } else {
+      result = num2
+   }
+   return result
+}
+
+func main() {
+  fmt.Println(max(1,2)) // 2
+}
